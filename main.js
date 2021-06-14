a8830497a87001649c7e112a134863e748b4638b
HaradaHelio <helioharada30@gmail.com>
2021-06-13 21:19:45 -0300

    finish dashboard layout and start profile

--- main.js
+++ main.js
@@ -1,46 +1,103 @@
-function createChart(id) {
-    const data = {
-        labels: [
-          'Venda',
-          'Não venda',
-     
+function createChart(element) {
+
+  // ### paramentros
+  const data = {
+    labels: [
+      'Venda',
+      'Não venda',
+
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [50, 12],
+      backgroundColor: [
+        '#6FCF97',
+        '#219653',
+      ],
+      hoverOffset: 4
+    }]
+  };
+  // ### Paramentros
+  const config = {
+    type: 'doughnut',
+    data,
+
+  };
+  // ### Criação do chart com pareametros
+  new Chart(
+    element,
+    config
+  );
+
+
+}
+
+function createBar(element) {
+
+  const labels = ['Motivo 1', 'Motivo 2', 'Motivo 3', 'motvivo 4']
+  const data = {
+    labels: labels,
+    datasets: [{
+      axis: 'y',
+      label: 'total',
+      fill: 'false',
+      data: [10, 8, 6, 4],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+
+      ],
+      borderWidth: 2
+    }]
+  };
+  const config = {
+    type: 'horizontalBar',
+    data,
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            ticks: {
+              beginAtZero: true
+            }
+          }
         ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [50, 12],
-          backgroundColor: [
-            '#6FCF97',
-            '#219653',
-   
-          ],
-          hoverOffset: 4
-        }]
-      };
+        yAxes: [
+          {
+            display: true,
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      },
+      indexAxis: 'y',
+    }
+  };
 
+  console.log(config)
+  // ### Criação do chart com pareametros
+  new Chart(
+    element,
+    config
+  );
 
+}
 
+//  ### Criação dos chart por ID 
+createChart(document.getElementById("myChart"))
 
+createChart(document.getElementById("myChart2"))
 
-      
-      const config = {
-        type: 'doughnut',
-        data,
-       
-      };
-
-
-
-    new Chart(
-        element,
-        config
-      );
-
-      
-  }
-
-  var element = document.getElementById("myChart")
-
-createChart(element)
-
-var element = document.getElementById("myChart2")
-createChart(element)
+createBar(document.getElementById("myBar"))
